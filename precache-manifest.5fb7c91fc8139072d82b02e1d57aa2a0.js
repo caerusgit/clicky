3c93b980439ddce524a96c7ab8f1b9b72edb25ab
Hugo Alberto Bravo Bonilla <hugo@Hugos-MacBook-Pro.local>
2020-02-04 14:38:59 -0600

    Updates

---
+++ precache-manifest.5fb7c91fc8139072d82b02e1d57aa2a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5778083c84350229119ef53eb2c5b382",
+    "url": "/clicky/index.html"
+  },
+  {
+    "revision": "de9d395037e1c980b21c",
+    "url": "/clicky/static/css/main.719df09f.chunk.css"
+  },
+  {
+    "revision": "a6951c8c6010ba72fa12",
+    "url": "/clicky/static/js/2.9943d154.chunk.js"
+  },
+  {
+    "revision": "ebbda8a36f26703e29f21e0f9ce480f4",
+    "url": "/clicky/static/js/2.9943d154.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de9d395037e1c980b21c",
+    "url": "/clicky/static/js/main.64b86af9.chunk.js"
+  },
+  {
+    "revision": "03845033a8934ae5952d",
+    "url": "/clicky/static/js/runtime-main.74279dd7.js"
+  }
+]);
