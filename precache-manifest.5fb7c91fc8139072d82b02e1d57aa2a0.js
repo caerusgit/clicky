d2042cd0af20611a7f1efa760a6fe4c55d38deb8
Hugo Alberto Bravo Bonilla <hugo@Hugos-MacBook-Pro.local>
2020-02-04 18:30:52 -0600

    Updates

--- precache-manifest.5fb7c91fc8139072d82b02e1d57aa2a0.js
+++ precache-manifest.5fb7c91fc8139072d82b02e1d57aa2a0.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8d318e9e452b5f973ed13ac312308644",
+    "revision": "5778083c84350229119ef53eb2c5b382",
     "url": "/clicky/index.html"
   },
   {
-    "revision": "1a539bee0cf7a1f2a15a",
+    "revision": "de9d395037e1c980b21c",
     "url": "/clicky/static/css/main.719df09f.chunk.css"
   },
   {
-    "revision": "1ea198f7285c65eecb1e",
-    "url": "/clicky/static/js/2.d869926e.chunk.js"
+    "revision": "a6951c8c6010ba72fa12",
+    "url": "/clicky/static/js/2.9943d154.chunk.js"
   },
   {
     "revision": "ebbda8a36f26703e29f21e0f9ce480f4",
-    "url": "/clicky/static/js/2.d869926e.chunk.js.LICENSE.txt"
+    "url": "/clicky/static/js/2.9943d154.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1a539bee0cf7a1f2a15a",
-    "url": "/clicky/static/js/main.adf77560.chunk.js"
+    "revision": "de9d395037e1c980b21c",
+    "url": "/clicky/static/js/main.64b86af9.chunk.js"
   },
   {
     "revision": "03845033a8934ae5952d",
